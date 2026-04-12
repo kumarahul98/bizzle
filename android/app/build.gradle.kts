506ecdd9a4b7889938f3ef0644ff34f424e32944
Rahul Kumar <rahulkumar@antstack.io>
2026-04-12 19:00:23 +0530

    fix(01): WR-01 remove debug-signed release buildType stub

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,14 +32,6 @@ android {
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
-
-    buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
-        }
-    }
 }
 
 flutter {
